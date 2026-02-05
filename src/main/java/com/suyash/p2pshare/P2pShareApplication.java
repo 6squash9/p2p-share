d660fe8c489f23ad496d0de0cb2dc3fdd0ebc57e
6squash9 <suyashmandaokar7@gmail.com>
2026-02-05 13:03:37 +0530

    init

---
+++ src/main/java/com/suyash/p2pshare/P2pShareApplication.java
@@ -0,0 +1,13 @@
+package com.suyash.p2pshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2pShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suyash/p2pshare/P2pShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyash.p2pshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2pShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
