0d6eeb09ec7d09eddc9811c45c579d13271c2f90
6squash9 <suyashmandaokar7@gmail.com>
2026-02-24 14:54:42 +0530

    feat: Set up client-side routing with React Router DOM and create initial Home and Room page components.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
-function App() {
-    return <div>P2P file sharer</div>
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RoomPage from "./pages/RoomPage";
 
+function App() {
+    return (
+        <Routes>
+            <Route path={"/"} element={<HomePage />} />
+            <Route path={"/room/:roomId"} element={<RoomPage />} />
+        </Routes>
+    )
 }
+
+
 
 export default App
