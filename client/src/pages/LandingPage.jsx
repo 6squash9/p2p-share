46a5a615df1019fc120b93b90c1a126ad5875adb
6squash9 <suyashmandaokar7@gmail.com>
2026-03-16 23:27:42 +0530

    Add hero section and styles to LandingPage

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -23,8 +23,17 @@ function LandingPage() {
 
             {/* hero section */}
             <section className='hero'>
+                <h1><span className="logo">Peer<span>Send</span></span> lets you send files privately.</h1>
 
+                <p>
+                    Send files directly to anyone. No middleman. No storage. No signup. Just fast, private sharing.
+                </p>
+                <div className='hero-buttons'>
+                    <button className='btn-primary'>Send a File →</button>
+                    <button className='btn-secondary'>How it works</button>
+                </div>
             </section>
+
         </div>
     )
 }
