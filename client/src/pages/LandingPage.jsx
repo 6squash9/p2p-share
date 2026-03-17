3a35fc4cd918f808266bd3e4dc635582f39b0cee
6squash9 <suyashmandaokar7@gmail.com>
2026-03-17 16:35:11 +0530

    Add hero video and smooth scroll effects

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,7 +1,42 @@
 import './LandingPage.css'
-
+import { useEffect, useRef } from 'react'
+import Lenis from 'lenis'
 
 function LandingPage() {
+    const videoTextRef = useRef(null)
+
+    // //lensi for smooth scroll
+    useEffect(() => {
+        // setting up lenis
+        const lenis = new Lenis({
+            lerp: 0.1,
+            smoothWheel: true,
+        })
+        // listening to scroll event
+        lenis.on('scroll', () => {
+            if (videoTextRef.current) {
+                const rect = videoTextRef.current.getBoundingClientRect()
+                const windowHeight = window.innerHeight
+                const distanceFromBottom = windowHeight - rect.top
+                const opacity = Math.min(Math.max(distanceFromBottom / 300, 0), 0.9)
+                const translateY = Math.max(20 - (distanceFromBottom / 15), 0)
+                videoTextRef.current.style.opacity = opacity
+                videoTextRef.current.style.transform = `translateX(-50%) translateY(${translateY}px)`
+            }
+        })
+        // animation loop (RAF) for lenis
+        function raf(time) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+
+        requestAnimationFrame(raf)
+
+        return () => lenis.destroy()
+    }, [])
+
+
+
     return (
         <div className='wrapper'>
             {/* navbar */}
@@ -31,6 +66,15 @@ function LandingPage() {
                 <div className='hero-buttons'>
                     <button className='btn-primary'>Send a File →</button>
                     <button className='btn-secondary'>How it works</button>
+                </div>
+                <div className='video-wrapper'>
+                    <video src="/Abstract+Objects.mp4"
+                        className="hero-video"
+                        autoPlay
+                        muted
+                        loop
+                        playsInline></video>
+                    <div className='video-text' ref={videoTextRef}>Peer<span>Send</span></div>
                 </div>
             </section>
 
